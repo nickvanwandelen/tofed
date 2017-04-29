f4058b617dddae2b5130314b0528427ba2f506f4
jbs2706 <jonahsneijders@gmail.com>
2017-04-29 18:28:50 +0200

    versie 1 beacon

--- index.ios.js
+++ index.ios.js
@@ -12,7 +12,7 @@ import {
   View
 } from 'react-native';
 
-export default class DummyApplication extends Component {
+export default class reactNativeBeaconExample extends Component {
   render() {
     return (
       <View style={styles.container}>
@@ -50,4 +50,4 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('DummyApplication', () => DummyApplication);
+AppRegistry.registerComponent('reactNativeBeaconExample', () => reactNativeBeaconExample);
